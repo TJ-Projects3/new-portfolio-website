f2a39191e47fed0683e3c07e1b9a333670fd8272
TJ-Projects3 <tjprojects3@gmail.com>
2025-09-30 14:56:50 -0400

    Added grid pattern and moved resume file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
             <nav>
                 <a href="/"><h2>home</h2></a>
                 <a href="#projects"><h2>projects</h2></a>
-                <a href="/TJO_Resume30.pdf" target="_blank"><h2>resume</h2></a>
+                <a href="/TJO_Resume30.pdf" target="_blank" rel="noopener noreferrer"><h2>resume</h2></a>
             </nav>
         </header>
     )
